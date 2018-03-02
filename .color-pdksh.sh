c465f50930c6391b92b99b3c00c924d41c14ac8c
Tobias Pape <tobias@netshed.de>
2018-03-02 14:44:01 +0100

    pdksh

--- .color-pdksh.sh
+++ .color-pdksh.sh
@@ -1,10 +1,10 @@
 #
 # for pdksh-shells
 _color=$1
-set -A COLORS --$(for i in $(jot $_colors 0 $_colors);\
+set -A COLORS $(for i in $(jot $_colors 0 $_colors);\
            do printf "$(tput setaf $i 0 0) ";\
            done)
-set -A BGCOLORS -- $(for  $(jot $_colors 0 $_colors);\
+set -A BGCOLORS $(for  $(jot $_colors 0 $_colors);\
              do printf "$(tput setab $i 0 0) ";\
              done))
 export COLORS

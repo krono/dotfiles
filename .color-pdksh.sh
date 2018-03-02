0e59dbead048573b3b839bdc342c5581f9529968
Tobias Pape <tobias@netshed.de>
2018-03-02 14:17:55 +0000

    Xolor

--- .color-pdksh.sh
+++ .color-pdksh.sh
@@ -1,10 +1,10 @@
 #
 # for pdksh-shells
-_colors=$(( $1 - 1 ))
-set -A COLORS $(for i in $(jot $_colors 0 $_colors);\
+_colors=$1
+set -A COLORS -- $(for i in $(jot $_colors 0);\
            do printf "$(tput setaf $i 0 0) ";\
            done)
-set -A BGCOLORS $(for  $(jot $_colors 0 $_colors);\
+set -A BGCOLORS -- $(for i in $(jot $_colors 0);\
              do printf "$(tput setab $i 0 0) ";\
              done)
 export COLORS

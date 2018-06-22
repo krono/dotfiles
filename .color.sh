68e2e6ce43bda9e701c9110356801c37d8fd736a
Tobias Pape <tobias@netshed.de>
2018-06-22 14:23:00 +0200

    color

--- .color.sh
+++ .color.sh
@@ -107,7 +107,7 @@ else
     EXTENDED=0
     export COLOR_BLACK="\[\033[30m\]"
     export COLOR_RED="\[\033[31m\]"
-    export COLOR_GREEN="\[\033[320m\]"
+    export COLOR_GREEN="\[\033[32m\]"
     export COLOR_YELLOW="\[\033[33m\]"
     export COLOR_BLUE="\[\033[34m\]"
     export COLOR_MAGENTA="\[\033[35m\]"
